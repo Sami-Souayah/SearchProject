c5706504000ae8e3f1f50a68e1baa30f084f90cf
Sami Souayah <souays@bu.edu>
2025-02-13 15:56:32 -0500

    Stuff

---
+++ indexpaths.py
@@ -0,0 +1,66 @@
+THE_INDEX = {
+
+    'dl19': 'msmarco-v1-passage',
+
+    'dl20': 'msmarco-v1-passage',
+
+    'covid': 'beir-v1.0.0-trec-covid.flat',
+
+    'arguana': 'beir-v1.0.0-arguana.flat',
+
+    'touche': 'beir-v1.0.0-webis-touche2020.flat',
+
+    'news': 'beir-v1.0.0-trec-news.flat',
+
+    'scifact': 'beir-v1.0.0-scifact.flat',
+
+    'fiqa': 'beir-v1.0.0-fiqa.flat',
+
+    'scidocs': 'beir-v1.0.0-scidocs.flat',
+
+    'nfc': 'beir-v1.0.0-nfcorpus.flat',
+
+    'quora': 'beir-v1.0.0-quora.flat',
+
+    'dbpedia': 'beir-v1.0.0-dbpedia-entity.flat',
+
+    'fever': 'beir-v1.0.0-fever-flat',
+
+    'robust04': 'beir-v1.0.0-robust04.flat',
+
+    'signal': 'beir-v1.0.0-signal1m.flat'
+    }
+
+
+THE_TOPICS = {
+
+    'dl19': 'dl19-passage',
+
+    'dl20': 'dl20-passage',
+
+    'covid': 'beir-v1.0.0-trec-covid-test',
+
+    'arguana': 'beir-v1.0.0-arguana-test',
+
+    'touche': 'beir-v1.0.0-webis-touche2020-test',
+
+    'news': 'beir-v1.0.0-trec-news-test',
+
+    'scifact': 'beir-v1.0.0-scifact-test',
+
+    'fiqa': 'beir-v1.0.0-fiqa-test',
+
+    'scidocs': 'beir-v1.0.0-scidocs-test',
+
+    'nfc': 'beir-v1.0.0-nfcorpus-test',
+
+    'quora': 'beir-v1.0.0-quora-test',
+
+    'dbpedia': 'beir-v1.0.0-dbpedia-entity-test',
+
+    'fever': 'beir-v1.0.0-fever-test',
+
+    'robust04': 'beir-v1.0.0-robust04-test',
+
+    'signal': 'beir-v1.0.0-signal1m-test'
+    }
