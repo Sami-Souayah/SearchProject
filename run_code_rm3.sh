581f44a21efedfcf76d1a7888c0792acb7715d1b
Sami Souayah <souays@bu.edu>
2025-02-21 23:40:13 -0500

    Stuff

--- run_code_rm3.sh
+++ run_code_rm3.sh
@@ -1,3 +1,6 @@
+datasets=('dl19' 'dl20' 'covid' 'arguana' 'touche' 'news' 'scifact' 'fiqa' 'scidocs' 'nfc' 'quora' 'dbpedia' 'fever' 'robust04' 'signal')
+
+
 for dataset in "${datasets[@]}"; do
     echo "Processing $dataset..."
     python3 bm25.py --dataset "$dataset" --k 1000 --rm3
