0062fcd02025a7965f520d84d918b87877faa583
Sami Souayah <souays@bu.edu>
2025-03-20 16:47:30 -0400

    Stuff

--- BERT/rerank.py
+++ BERT/rerank.py
@@ -15,85 +15,81 @@ directory = '/home/gridsan/ssouayah/BM25Output'
 OutputDir = '/home/gridsan/ssouayah/BERTOutput'
 chunk_size = 500
 model_name = '/home/gridsan/ssouayah/ms-marco-MiniLM-L6-v2'
+# This is minimal code to get a reranker running. You'll need to fill in the blanks, but this is
+# the structure you need for the code.
 
-class FetchText():
-    def __init__(self,dataset):
-        self.dataset = dataset
-        self.topics = get_topics(THE_TOPICS[self.dataset] if self.dataset != 'dl20' else 'dl20')
-        self.qid = 0
-        self.tokenized_text = {}
-        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
-        self.directory = f"/home/gridsan/ssouayah/BM25Output/{dataset}_run.csv"
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.final_scores = {}
-    
-    def FetchText(self, docid, qid):
-        searcher =  LuceneSearcher.from_prebuilt_index(THE_INDEX[self.dataset])
-        doc = searcher.doc(docid)
-        json_doc = json.loads(doc.raw())
-        if self.dataset == 'dl19' or self.dataset == 'dl20':
-            text = json_doc['contents']
-        else:
-            if 'title' in json_doc:
-                text = f"{json_doc['title']} {json_doc['text']}"
-        self.tokenized_text[docid] = self.tokenize_text(text, qid)
 
-    def ReadCSV(self):
-        input_filename = os.path.join(directory, f'{self.dataset}_run.csv')
-        for chunk in pd.read_csv(input_filename, delimiter=' ', chunksize=chunk_size):
-             for _, row in chunk.iterrows():
-                self.qid = int(row.iloc[0])
-                docID = row.iloc[2]
-                self.FetchText(docID, self.qid)
-
-    def tokenize_text(self, text, qid):
-        qid = int(qid)
-        query = self.topics[qid]['title']
-        encoding = self.tokenizer(
-            text, query,
-            padding=True, 
-            truncation=True, 
-            return_tensors="pt", 
-        )
-        return encoding
-    
-    def ModelEval(self):
-        model = self.model
-        model.eval()
-        for i in self.tokenized_text:
-            with torch.no_grad():
-                scores = model(**self.tokenized_text[i]).logits.item()
-            self.final_scores[i] = scores
-        self.final_scores = dict(sorted(self.final_scores.items(), key=lambda x: x[1], reverse=True))
-
-    def WriteToFile(self):
-        output_filename = os.path.join(OutputDir, f'{self.dataset}_bert.csv')
-        with open(output_filename, 'w', newline='') as file:
-            count=0
-            for i in self.final_scores:
-                docid = i
-                query = self.topics[self.qid]['title']
-                rank = self.final_scores[i]
-                count+=1
-                file.write(f'{self.qid} Q0 {docid} {count} {rank} rank \n')
+# We need a function that takes in the docid from the bm25 retrival run and outputs
+# the document text. 
+def load_text(dataset, searcher, docid): 
+    doc = searcher.doc(docid)
+    json_doc = json.loads(doc.raw())
+    if dataset == 'dl19' or dataset == 'dl20':
+        text = json_doc['contents']
+    else:
+        if 'title' in json_doc:
+            text = f"{json_doc['title']} {json_doc['text']}"
+    return text 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', help = 'Input dataset', type=str)
 
+
     args = parser.parse_args()
-    if args.dataset:
-        data =args.dataset 
+    dataset = args.dataset
+    bm25_run = pd.read_csv(f'/home/gridsan/ssouayah/BM25Output/{dataset}_run.csv', delimiter=' ', dtype=str)
+    bm25_run.columns = ['qid', 'q0', 'docid', 'rank', 'score','extra','extra']
+    searcher =  LuceneSearcher.from_prebuilt_index(THE_INDEX[dataset])
+    topics = get_topics(THE_TOPICS[dataset] if dataset != 'dl20' else 'dl20')
+    topics = {str(key): value for key, value in topics.items()}    
+    qrels = get_qrels(THE_TOPICS[dataset])
+    model = AutoModelForSequenceClassification.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    fetch = FetchText(data)
-    fetch.ReadCSV()
-    fetch.ModelEval()
-    fetch.WriteToFile()
-    qrels = get_qrels(THE_TOPICS[data])
-    print(os.system(f"python -m pyserini.eval.trec_eval -c -m recall.100 {THE_TOPICS[data]} '/home/gridsan/ssouayah/BERTOutput/{data}_bert.csv'"))
+    reranked_run = [] 
+    for qid in topics:
+        qid = str(qid)
+        # Fetch BM25 results for the given topic
+        query = topics[qid]['title']
+        bm25_query_results = bm25_run[bm25_run['qid'] == qid]
+        # This should be 1000 x num_columns....
+        query_reranked = [] 
+        for id, row in bm25_query_results.iterrows():
+            docid = row['docid']
+            document_text = load_text(dataset, searcher, docid)
+
+            encoding = tokenizer(
+                            query, document_text, 
+                            padding=True, 
+                            truncation=True, 
+                            return_tensors="pt", 
+                    )
+            
+            # We need the score of only the positive label, usually at index 1.
+            # 0 = Prob of non-relevant
+            # 1 = Prob of relevant
+            prob_relevant = model(**encoding).logits.item()
+            
+            query_reranked.append([qid, docid, prob_relevant])
+        print(query_reranked[2])
+
+        # TODO: sort query reranked by score in decending order and append it to reranked_run
+       # sorted_query_reranked = <Fill this in...
+        sorted_query_reranked = query_reranked.sort(key=lambda x: x[2], reverse=True)
+        reranked_run.append(sorted_query_reranked)
 
 
+
+    # TODO: Now take reranked run, flatten it, and write it to a file...
+
+    with open(output_filename, 'w', newline='') as file:
+        for query_results in reranked_run:
+            rank = 0
+            for document in query_results:
+                file.write(f'{document[0]} Q0 {document[1]} {rank} {document[2]} rank \n')
+                rank += 1
 
 
 
