a7b0ec795dc90a9a7013ce1245ff49d768f5aea1
Sami Souayah <souays@bu.edu>
2025-03-27 17:19:40 -0400

    Stuff

--- BM25/run_code.sh
+++ BM25/run_code.sh
@@ -1,7 +1,7 @@
 #Original dataset: 'dl19' 'dl20' 'covid' 'arguana' 'touche' 'news' 'scifact' 'fiqa' 'scidocs' 'nfc' 'quora' 'dbpedia' 'fever' 'robust04' 'signal'
 #Dataset variable my be changed to reflect what datasets are needed
 
-datasets=('quora' 'dbpedia' 'fever' 'robust04' 'signal')
+datasets=('robust04' 'signal')
 
 for dataset in "${datasets[@]}"; do
     echo "Processing $dataset..."
