f960733d2c10f5e743a7338c27d6f72cf05bbb6f
Sami Souayah <souays@bu.edu>
2025-04-10 20:45:40 -0400

    Stuff

--- BERT/run_bert.sh
+++ BERT/run_bert.sh
@@ -3,7 +3,7 @@ export HF_HOME=/tmp/huggingface
 #Full dataset: 'dl19' 'dl20' 'covid' 'touche' 'news' 'scifact' 'fiqa' 'scidocs' 'nfc' 'dbpedia' 'robust04''
 # Datasets variable under may be modified to reflect datasets already processed. 
 
-datasets=('touche' 'news' 'scifact' 'fiqa' 'scidocs' 'nfc' 'quora' 'dbpedia' 'fever' 'robust04' 'signal')
+datasets=('scifact' 'fiqa' 'scidocs' 'nfc' 'quora' 'dbpedia' 'fever' 'robust04' 'signal')
 
 for dataset in "${datasets[@]}"; do
     echo "Processing $dataset..."
