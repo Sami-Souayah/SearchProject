fe8a882b206f09c9ad013c7668adf3b5cc36df45
Sami Souayah <souays@bu.edu>
2025-04-11 19:34:03 -0400

    Stuff

---
+++ BERT/run_sntnc.sh
@@ -0,0 +1,13 @@
+export HF_HOME=/tmp/huggingface
+
+#Full dataset: 'dl19' 'dl20' 'covid' 'touche' 'news' 'scifact' 'fiqa' 'scidocs' 'nfc' 'dbpedia' 'robust04''
+# Datasets variable under may be modified to reflect datasets already processed. 
+
+datasets=('dl19')
+
+for dataset in "${datasets[@]}"; do
+    echo "Processing $dataset..."
+    python sentencetransformers.py --dataset "$dataset"
+done
+
+echo "All datasets processed!"
