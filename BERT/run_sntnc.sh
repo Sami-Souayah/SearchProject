c5d206b4e2d0a23e84038dccc2d0f2dca4885441
Sami Souayah <souays@bu.edu>
2025-04-17 17:43:55 -0400

    Stuff

--- BERT/run_sntnc.sh
+++ BERT/run_sntnc.sh
@@ -3,7 +3,7 @@ export HF_HOME=/tmp/huggingface
 #Full dataset: 'dl19' 'dl20' 'covid' 'touche' 'news' 'scifact' 'fiqa' 'scidocs' 'nfc' 'dbpedia' 'robust04''
 # Datasets variable under may be modified to reflect datasets already processed. 
 
-datasets=('dl19')
+datasets=('scidocs' 'nfc' 'dbpedia' 'robust04')
 
 for dataset in "${datasets[@]}"; do
     echo "Processing $dataset..."
